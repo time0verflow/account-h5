0bc2fbcef588250dc3d252abf260789b2d325a4c
time0verflow <13121125012@163.com>
2022-02-21 16:47:12 +0800

    '自动部署'

---
+++ ecosystem.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+    apps: [
+        {
+            name: 'account-h5',
+            script: 'account-h5-back'
+        }
+    ],
+    deploy: {
+        production: {
+            user: 'root',
+            host: '47.108.229.144',
+            ref: 'origin/master',
+            repo: 'https://github.com/time0verflow/account-h5.git',
+            path: '/workspace/account-h5',
+            'post-deploy': 'git reset --hard && git checkout master && git pull && npm i --production=false && npm run build:release && pm2 startOrReload ecosystem.config.js', // -production=false 下载全量包
+            env: {
+                NODE_ENV: 'production'
+            }
+        }
+    }
+}
